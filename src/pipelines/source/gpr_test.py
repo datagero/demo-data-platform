43fe891fe01eb0b85cdb453a83a4f6f6e3ae4f6b
datagero <datagero@outlook.com>
2024-09-04 14:29:54 -0600

    renamed code to src

--- src/pipelines/source/gpr_test.py
+++ src/pipelines/source/gpr_test.py
@@ -159,7 +159,7 @@ def ingest_pipeline(schema_path, categorized_file_path, output_path):
 
 if __name__ == '__main__':
     # Example usage
-    schema_path = 'vip_sci/schemas/source/gpr/schemas.json'
-    categorized_file_path = 'vip_sci/data_profiling/categorized_files/categorized_files_gpr.json'
+    schema_path = 'src/schemas/source/gpr/schemas.json'
+    categorized_file_path = 'src/data_profiling/categorized_files/categorized_files_gpr.json'
     output_path = 'datalake/bronze/'
     ingest_pipeline(schema_path, categorized_file_path, output_path)
